ed12a8b2f8ef28c81ff2eb0d59266519e1da4336
tsikoandrii <cikoandrii3@gmail.com>
2022-07-19 23:40:25 +0300

    Atomic design refactoring

---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+  width: 100%;
+  padding: 20px;
+  background: #c4c4c4;
+`
+
+const Header = () => {
+  return <StyledHeader>Header</StyledHeader>
+}
+
+export default Header
